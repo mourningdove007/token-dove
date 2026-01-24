a188fc041e903d54b56a590a8f8d57106e7e6af0
Mourning Dove <mourningdove007@gmail.com>
2026-01-24 15:17:20 -0600

    styling concerns

--- src/pages/SignOn.svelte
+++ src/pages/SignOn.svelte
@@ -11,7 +11,14 @@
     };
 </script>
 
-<button onclick={connectWallet}>Sign In</button>
+<h2>Token Dove</h2>
+<p>This is an <a href="https://github.com/mourningdove007/token-dove">open source project</a> with the end goal of providing videogame developers an easy platform for in game asset exchange. Currently, the app will allow a user to sign in with their Phantom wallet and view transaction history.</p>
+<button onclick={connectWallet}>
+    <img src="src/assets/phantom.ico" alt="Phantom" class="logo" />
+    <span>Continue with Phantom</span>
+</button>
+<p>Privacy Disclaimer: No personal information is collected as there is no backend. All information is owned by the user.</p>
+
 
 <div class="footer">
     <a href="https://github.com/mourningdove007/token-dove" target="_blank"
@@ -21,11 +28,20 @@
 
 <style>
     button {
+        border-width: 2px;
+        border-color: rgb(171, 159, 242);;
+        gap: 0.5rem;
+        display: inline-flex;
+        align-items: center;
+        line-height: 1;
         padding: 0.6rem;
         font-size: 1rem;
-        border: none;
         border-radius: 4px;
         cursor: pointer;
+    }
+
+    p {
+        width: 500px;
     }
 
     .footer {
