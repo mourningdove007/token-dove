fabbb6114662188f012c81e52ae2a14aa8f89294
mourningdove007 <mourningdove007@gmail.com>
2026-01-31 14:32:43 -0600

    create token account

--- on-chain/programs/on-chain/src/lib.rs
+++ on-chain/programs/on-chain/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenInterface};
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
 declare_id!("DPxJ2kZv6z2gb8gDrFUkftmMQGfAqR3c4XFbC9SY9L9n");
 
@@ -13,6 +13,14 @@ mod hello_anchor {
 
     pub fn create_mint(ctx: Context<CreateMint>) -> Result<()> {
         msg!("Created Mint Account: {:?}", ctx.accounts.mint.key());
+        Ok(())
+    }
+
+    pub fn create_token_account(ctx: Context<CreateTokenAccount>) -> Result<()> {
+        msg!(
+            "Created Token Account: {:?}",
+            ctx.accounts.token_account.key()
+        );
         Ok(())
     }
 }
@@ -29,6 +37,26 @@ pub struct CreateMint<'info> {
         seeds = [b"mint"],
         bump
     )]
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreateTokenAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+
+        init_if_needed,
+        payer = signer,
+        token::mint = mint,
+        token::authority = token_account,
+        token::token_program = token_program,
+        seeds = [b"token"],
+        bump
+    )]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
     pub mint: InterfaceAccount<'info, Mint>,
     pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
--- on-chain/tests/on-chain.ts
+++ on-chain/tests/on-chain.ts
@@ -1,7 +1,11 @@
 import * as anchor from "@anchor-lang/core";
 import { Program } from "@anchor-lang/core";
 import { HelloAnchor } from "../target/types/hello_anchor";
-import { TOKEN_2022_PROGRAM_ID, getMint } from "@solana/spl-token";
+import {
+  TOKEN_2022_PROGRAM_ID,
+  getMint,
+  getAccount,
+} from "@solana/spl-token";
 
 describe("hello_anchor", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -11,8 +15,14 @@ describe("hello_anchor", () => {
     [Buffer.from("mint")],
     program.programId,
   );
+
+  const [token, tokenBump] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("token")],
+    program.programId,
+  );
+
   it("Is initialized!", async () => {
-    
+
     const tx = await program.methods.testInstruction().rpc();
     console.log("Your transaction signature", tx);
   });
@@ -35,4 +45,29 @@ describe("hello_anchor", () => {
 
     console.log("Mint Account", mintAccount);
   })
+
+
+  it("can create token account", async () => {
+    const tx = await program.methods
+      .createTokenAccount()
+      .accounts({
+        mint: mint,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+      })
+      .rpc({ commitment: "confirmed" });
+
+    console.log("Your transaction signature", tx);
+
+    const tokenAccount = await getAccount(
+      program.provider.connection,
+      token,
+      "confirmed",
+      TOKEN_2022_PROGRAM_ID,
+    );
+
+    console.log("Token Account", tokenAccount);
+  });
+
+
+
 });
