21280f57a250723e64465d1b23c36c71f7761143
mourningdove007 <mourningdove007@gmail.com>
2026-02-15 16:44:14 -0600

    coming soon create page

--- src/pages/Create.svelte
+++ src/pages/Create.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import { Ghost } from "lucide-svelte";
-      import GlowingToken from "../lib/GlowingToken.svelte";
+    import GlowingToken from "../lib/GlowingToken.svelte";
 
 </script>
 
-<GlowingToken IconComponent={Ghost} label={"Coming Soon"}/>
+<GlowingToken size={200} IconComponent={Ghost} label={"Coming Soon"}/>
 
